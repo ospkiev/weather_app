ad5edb9a4814df74c56be4dd7f8dd43ff493b45b
Pavel Karpenko <ospkiev@gmail.com>
2019-01-17 11:24:52 +0200

    Updates

---
+++ precache-manifest.4cfb26649d565de23723c77652b8c5fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d31963855c634ba8695ceba129ed6faf",
+    "url": "/weather_app/static/media/Harwen-Simple-Favorites.d3196385.ico"
+  },
+  {
+    "revision": "2b8e50d38204ea6f6971",
+    "url": "/weather_app/static/js/runtime~main.2b8e50d3.js"
+  },
+  {
+    "revision": "905cc0370d895f9b492d",
+    "url": "/weather_app/static/js/main.905cc037.chunk.js"
+  },
+  {
+    "revision": "a31bf61ba8ffe292c950",
+    "url": "/weather_app/static/js/1.a31bf61b.chunk.js"
+  },
+  {
+    "revision": "905cc0370d895f9b492d",
+    "url": "/weather_app/static/css/main.7befa588.chunk.css"
+  },
+  {
+    "revision": "a31bf61ba8ffe292c950",
+    "url": "/weather_app/static/css/1.cdc1b7b5.chunk.css"
+  },
+  {
+    "revision": "7263e4fcbaf3278e63e517f9d757a33d",
+    "url": "/weather_app/index.html"
+  }
+];
